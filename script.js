9d21b53c5120845ee0de9bd7bbf6a955f766013c
apoorva10on10redheadbaddie <apoorvasahu2806@gmail.com>
2025-01-11 08:50:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
